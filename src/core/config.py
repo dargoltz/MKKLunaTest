ed257a2806002daf21f66fc3fb150c2e16f826b0
andrey.lesnov <andreythecoolguy@gmail.com>
2026-02-14 16:05:24 +0300

    refactor: db url

--- src/core/config.py
+++ src/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    DB_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/mkklunatest"
+    DB_URL: str = "postgres:postgres@localhost:5432/postgres"
     API_KEY: str = "SERIOUS_CYBER_DEFENSE"
 
 config = Config()
