1b456801b52ea12902730d8489c7a696c44dfa54
andrey.lesnov <andreythecoolguy@gmail.com>
2026-02-14 14:24:17 +0300

    add default db url

--- src/core/config.py
+++ src/core/config.py
@@ -2,6 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    DB_URL: str = ... # todo
+    DB_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/mkklunatest"
 
 config = Config()
